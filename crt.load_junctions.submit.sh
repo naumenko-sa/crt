3abfbbdb8fe5f3e4cf0138aa2710486eff4675f5
naumenko-sa <evolgenomicslab@gmail.com>
2017-12-11 16:02:25 -0500

    new file:   crt.load_junctions.submit.sh

---
+++ crt.load_junctions.submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+bam=`head -n1 table.txt`
+prev_id=`qsub ~/crt/crt.load_junctions.localhd.pbs -v bam=$bam`
+
+for bam in `cat table.txt | sed 1d`;
+do
+    job_id=`qsub ~/crt/crt.load_junctions.localhd.pbs -W depend=afterok:$prev_id -v bam=$bam`
+    echo $bam $prev_id $job_id
+    prev_id=$job_id
+done
