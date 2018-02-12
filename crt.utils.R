84d62d9f02e041efa0a544661d41905d8e7f356f
naumenko-sa <evolgenomicslab@gmail.com>
2018-02-12 11:29:01 -0500

    modified:   crt.utils.R

--- crt.utils.R
+++ crt.utils.R
@@ -194,11 +194,37 @@ merge_row_names = function(df1,df2)
 mds_plot = function()
 {
     library(edgeR)   
-    counts = read.feature_counts_dir(TRUE)
-    group = factor(c(rep(1,ncol(counts))))
+    counts = read.feature_counts_dir(F)
+    #group = factor(c(rep(1,ncol(counts))))
+    
+    group = factor(c("F","F","F","M","M","Myo","M","M","M","M","Mvl","M","Myo","Myo","F","F",
+                   "F","F","F","F","F","Myo","F","Myo","F","F","F","F","F","F","F","F","M",
+                   "M","Myo","M","Myo","M","Myo","Myo","Myo","Myo","M","M","Myo","F","F","F","F"))    
+    
+    v_colors = c()
+    for (i in 1:length(group))
+    {
+            if(group[i] == "F"){
+                clr = "orange"
+            }else if (group[i] == "Myo"){
+                clr = "red"
+            }else{
+                clr = "darkgreen"
+            }
+        v_colors[i] = clr
+    }
+    
     y=DGEList(counts=counts,group=group,remove.zeros = T)
     png("mds.png",res=300,width=2000,height=2000)
-    plotMDS(y,cex=0.7,srt=0,xlim=c(-6,8),ylim=c(-8,5))
+    
+    mds = plotMDS(y,
+            labels=NULL
+    )
+    plot(mds,
+         col = v_colors,
+         pch=19,xlab = "MDS dimension 1", 
+         ylab = "MDS dimension 2")
+    
     dev.off()
 }
 
@@ -612,5 +638,66 @@ all_expression_dotplots = function()
     for (gene_panel_name in panel_list)
     {
         expression_dotplot(gene_panel_name)
+    }
+}
+
+# plots coverage for every gene for all samples having sample.coverage in the current dir - output of
+# bam.coverage.bamstats05.sh
+coverage_plot = function ()
+{
+    setwd("~/Desktop/work")
+    files = list.files(".","\\.coverage$")
+    #samples = unlist(read.table("samples.txt", stringsAsFactors=F))
+    
+    coverage = read.delim(files[1],header=T,stringsAsFactors = F)
+    coverage = coverage[,c("gene","avg")]
+    colnames(coverage)[2]=files[1]
+    
+    for (file in tail(files,-1))
+    {
+        sample_coverage = read.delim(file,header=T,stringsAsFactors = F)
+        sample_coverage = sample_coverage[,c("gene","avg")]
+        colnames(sample_coverage)[2]=file
+        coverage = cbind(coverage,sample_coverage[2])
+    }
+    row.names(coverage) = coverage$gene
+    coverage$gene=NULL
+    
+    #remove ACTA1 with has coverage of 328115
+    #coverage = coverage[-c(1),]
+    
+    n_genes = nrow(coverage)
+
+    bin1 = c()
+    bin2 = c()
+    bin3 = c()
+    bin4 = c()
+    bin5 = c()
+    
+    for (i in 1:n_genes)
+    {
+        mean_expression = rowMeans(coverage[i,])
+        gene = row.names(coverage)[i]
+        #print(paste0(gene," ",mean_expression))
+        if (mean_expression >= 10000){
+            bin1 = unique(sort(c(bin1,gene)))
+        }else if (mean_expression >= 1000){
+            bin2 = unique(sort(c(bin2,gene)))
+        }else if (mean_expression >= 100){
+            bin3 = unique(sort(c(bin3,gene)))
+        }else if (mean_expression >= 10){
+            bin4 = unique(sort(c(bin4,gene)))
+        }else{
+            bin5 = unique(sort(c(bin5,gene)))
+        }
+    }
+    
+    for (bin in (c("bin1","bin2","bin3","bin4","bin5")))
+    {
+        png(paste0(bin,".png"),res=300,width=5000,height=2000)
+        boxplot(t(coverage[get(bin),]),
+            las=2,
+            cex.axis=0.8)
+        dev.off()
     }
 }
