cd5dce3f2416125f0d01ae00d6786745459d1d42
naumenko-sa <evolgenomicslab@gmail.com>
2018-04-17 22:28:25 -0400

    modified:   crt.gtex_junctions.R

--- crt.gtex_junctions.R
+++ crt.gtex_junctions.R
@@ -2,17 +2,21 @@
 #parse the gct file with junction counts
 #output the list of non-zero junctions with sum junction counts
 
+args = commandArgs(trailingOnly = T)
+
 #setwd("~/Desktop/work")
 
 #download the list of samples - works only from browser - biobank inventory:
 #https://www.gtexportal.org/home/datasets
 
-biobank = read.delim("biobank_collection_20180417_070153.txt", stringsAsFactors=F)
+#biobank = read.delim("biobank_collection_20180417_070153.txt", stringsAsFactors=F)
+biobank = read.delim(args[1],stringsAsFactors=F)
 biobank = biobank[biobank$materialType=="RNA:Total RNA",]
 biobank = biobank[biobank$tissueSiteDetail=="Muscle - Skeletal",]
 samples = gsub("-",".",biobank$tissueSampleId)
 
-GTEx_junctions = read.delim("GTEx_junctions.txt", skip = 2, stringsAsFactors=FALSE)
+#GTEx_junctions = read.delim("GTEx_junctions.txt", skip = 2, stringsAsFactors=FALSE)
+GTEx_jucntions = read.delim(args[2], skip = 2, stringsAsFactors=F)
 cnames = colnames(GTEx_junctions)
 for (i in 3:length(cnames))
 {
