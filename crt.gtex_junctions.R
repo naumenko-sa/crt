214fd28d16810d85b3aa9005e4c63592a46e6231
naumenko-sa <evolgenomicslab@gmail.com>
2018-04-17 23:16:15 -0400

    modified:   crt.gtex_junctions.R

--- crt.gtex_junctions.R
+++ crt.gtex_junctions.R
@@ -15,24 +15,42 @@ biobank = biobank[biobank$materialType=="RNA:Total RNA",]
 biobank = biobank[biobank$tissueSiteDetail=="Muscle - Skeletal",]
 samples = gsub("-",".",biobank$tissueSampleId)
 
+print(length(samples))
+
 #GTEx_junctions = read.delim("GTEx_junctions.txt", skip = 2, stringsAsFactors=FALSE)
-GTEx_jucntions = read.delim(args[2], skip = 2, stringsAsFactors=F)
+GTEx_junctions = read.delim(args[2], skip = 2, stringsAsFactors=F)
+
+# there are duplicates - same junction, same counts, two different genes, 
+# i.e. 1_33060825_33065687 
+# ENSG00000254553.1
+# ENSG00000160062.10
+
+GTEx_junctions$Description=NULL
+GTEx_junctions = unique(GTEx_junctions)
+
 cnames = colnames(GTEx_junctions)
-for (i in 3:length(cnames))
+for (i in 2:length(cnames))
 {
     cnames[i] = substr(cnames[i],1,15)
 }
 
 colnames(GTEx_junctions) = cnames
 
+print("Columns renamed")
+
 #not all muscular samples present in junction table
 #sample present in junctions: GTEX.11ZUS.1426
 #sample absent in junctions: GTEX.S33H.2226
 samples = samples[samples %in% colnames(GTEx_junctions)]
 
+print("Samples in calculated")
+
 junction_ids = GTEx_junctions$junction_id
 
 GTEx_junctions = subset(GTEx_junctions,select = samples)
+
+print("Junctions subsetted")
+
 GTEx_counts = rowSums(GTEx_junctions)
 
 GTEx = data.frame(row.names = junction_ids)
