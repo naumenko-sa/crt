3c07c5533ab4b0a85b38714d6e877f83a05ac70f
naumenko-sa <evolgenomicslab@gmail.com>
2018-06-22 12:50:11 -0400

    modified:   crt.variants.trios.sh

--- crt.variants.trios.sh
+++ crt.variants.trios.sh
@@ -4,5 +4,5 @@
 
 for f in *.panel.csv;
 do
-    cat $f | sed 1d | awk -F '","' '{if ($13>=10) print $1"-"$3"-"$4}' | sed s/\"//g | sort > $f.vars
+    cat $f | sed 1d | awk -F '","' '{if ($13>=10 && length($3)==1 && length($4)==1) print $1"-"$3"-"$4}' | sed s/\"//g | sort > $f.vars
 done
