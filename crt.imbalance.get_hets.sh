3081077613124a35ea77875a5282f3ad4c3c68fe
naumenko-sa <evolgenomicslab@gmail.com>
2018-10-24 11:28:56 -0400

    modified:   crt.bcbio.default.yaml 	modified:   crt.imbalance.get_hets.sh

--- crt.imbalance.get_hets.sh
+++ crt.imbalance.get_hets.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# extracts variants in protein coding genes for allelic imbalance analysis
+# extract variants in protein coding genes for allelic imbalance analysis
 
-echo "chrom,pos,ref,alt" > $1.het.csv
+if [ -z $vcf ]
+then
+    vcf=$1
+fi
 
-bcftools view -v snps -R ~/cre/data/protein_coding_genes.exons.bed $1 | \
-bcftools query -i 'FMT/GT="0/1" && INFO/DP>=20 && INFO/possible_rnaedit=0 && FILTER="PASS"' -f '%CHROM,%POS,[%AD]\n' - >> $1.het.csv
+bname=`basename $vcf .vcf.gz`
+
+echo "chrom,pos,ref,alt" > $bname.het.csv
+
+bcftools view -v snps -R ~/cre/data/protein_coding_genes.exons.bed $vcf | \
+bcftools query -i 'FMT/GT="0/1" && INFO/DP>=20 && INFO/possible_rnaedit=0 && FILTER="PASS"' -f '%CHROM,%POS,[%AD]\n' - >> $bname.het.csv
