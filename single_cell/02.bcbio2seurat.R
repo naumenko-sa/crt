f42c676229543c93b87531bf7214e5238f957401
naumenko.sa <evolgenomicslab@gmail.com>
2020-01-03 11:30:32 -0500

    sync

--- single_cell/02.bcbio2seurat.R
+++ single_cell/02.bcbio2seurat.R
@@ -4,7 +4,16 @@
 # which R
 # Rscript 00_create_seurat_object.R
 # conda deactivate
+# should have 3 files from bcbio
+file.rename("tagcounts.mtx", "matrix.mtx")
+file.rename("tagcounts.mtx.rownames", "features.tsv")
+file.rename("tagcounts.mtx.colnames", "barcodes.tsv")
+
+gzip("matrix.mtx")
+gzip("features.tsv")
+gzip("barcodes.tsv")
+
 library(Seurat)
-counts <- Read10X(data.dir = "data", gene.column = 1)
+counts <- Read10X(data.dir = ".", gene.column = 1)
 seurat_object <- CreateSeuratObject(counts = counts, min.features = 100)
 saveRDS(seurat_object, "seurat.bcbio.RDS")
