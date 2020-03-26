af73de96779bc8dfccc333ce8ed116dc3e2b0c9a
naumenko.sa <evolgenomicslab@gmail.com>
2020-03-26 15:24:57 -0400

    sync

--- velocity/kallisto_bus.sh
+++ velocity/kallisto_bus.sh
@@ -7,23 +7,30 @@
 #SBATCH --time=2-00:00              # Runtime in D-HH:MM format, 10:00:00 for hours
 #SBATCH --job-name=kallisto          # Job name
 #SBATCH -c 8			    # cores
-#SBATCH --mem=10G           # Memory needed per CPU or --mem-per-cpu
+#SBATCH --mem=100G           # Memory needed per CPU or --mem-per-cpu
 #SBATCH --output=project_%j.out     # File to which STDOUT will be written, including job ID
 #SBATCH --error=project_%j.err      # File to which STDERR will be written, including job ID
 #SBATCH --mail-type=ALL             # Type of email notification (BEGIN, END, FAIL, ALL)
 
 date
-which kallisto
+
+#conda activate kbenv
+which kb
 
 # https://bustools.github.io/BUS_notebooks_R/velocity.html
 
+kreference_prefix=/n/data1/cores/bcbio/naumenko/velocity_test/output/neuron10k_velocity
+
 # 100G ram
-#kallisto bus \
-#-i ../output/mm_cDNA_introns_97.idx \
-#-o ../output/neuron10k_velocity \
-#-x 10xv3 \
-#-t 8 \
-#neuron_10k_v3_S1_L002_R1_001.fastq.gz neuron_10k_v3_S1_L002_R2_001.fastq.gz \
-#neuron_10k_v3_S1_L001_R1_001.fastq.gz neuron_10k_v3_S1_L001_R2_001.fastq.gz
+kb count \
+-i ${kreference_prefix}/mm_cDNA_introns_97.idx \
+-g ${kreference_prefix}/neuron10k_velocity/tr2g.tsv \
+-x INDROPSV3 \
+-o kallisto_bus_output \
+-c1 ${kreference_prefix}/cDNA_tx_to_capture.txt \
+-c2 ${kreference_prefix}/introns_tx_to_capture.txt \
+--lamanno \
+-t 8 \
+${1}_2.fq.gz ${1}_4.fq.gz ${1}_1.fq.gz
 
 date
